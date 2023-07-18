5b64f26d1838dbc579e431e856840bac1317cbef
larryshank <larry113@gmail.com>
2023-07-17 20:20:10 -0600

    UPDATE: begin conversion to .mdx from .md

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 const getPostContent = (slug: string) => {
   const folder = "src/posts/";
-  const file = `${folder}${slug}.md`;
+  const file = `${folder}${slug}.mdx`;
   const content = fs.readFileSync(file, "utf8");
   const matterResult = matter(content);
   return matterResult;
--- src/helpers/getPostMetadata.ts
+++ src/helpers/getPostMetadata.ts
@@ -5,9 +5,8 @@ import { PostMetadata } from "../types/PostMetadata";
 const getPostMetadata = (): PostMetadata[] => {
   const folder = "src/posts/";
   const files = fs.readdirSync(folder);
-  const markdownPosts = files.filter((file) => file.endsWith(".md"));
-  // const slugs = markdownPosts.map((file) => file.replace(".md", ""));
-  // return slugs;
+  const markdownPosts = files.filter((file) => file.endsWith(".mdx"));
+
   const posts = markdownPosts.map((fileName) => {
     const fileContents = fs.readFileSync(`src/posts/${fileName}`, "utf8");
     const matterResult = matter(fileContents);
@@ -15,7 +14,7 @@ const getPostMetadata = (): PostMetadata[] => {
       title: matterResult.data.title,
       date: matterResult.data.date,
       subtitle: matterResult.data.subtitle,
-      slug: fileName.replace(".md", ""),
+      slug: fileName.replace(".mdx", ""),
     };
   });
 
